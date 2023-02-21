0779546b455a9d65c7c2dbf86cd71d6c015f87ff
pupungeggang289 <kyongkyong2892@gmail.com>
2023-02-21 22:41:20 +0900

    0.0.0

--- Game/const.py
+++ Game/const.py
@@ -1,3 +1,8 @@
 class Color():
     black = (0, 0, 0)
     white = (255, 255, 255)
+
+class Font():
+    title = None
+    main_1 = None
+    main_2 = None
--- Game/main.py
+++ Game/main.py
@@ -1,7 +1,9 @@
 import pygame
 import sys
 
+import asset
 import var
+import const
 
 import title
 
@@ -10,6 +12,18 @@ def init():
     var.screen = pygame.display.set_mode(var.screen_size)
     pygame.display.set_caption('Card Game Sample')
     var.clock = pygame.time.Clock()
+
+    load_font()
+    load_image()
+
+def load_font():
+    pygame.font.init()
+    const.Font.title = pygame.font.Font('Font/neodgm.ttf', 32)
+    const.Font.main_1 = pygame.font.Font('Font/neodgm.ttf', 24)
+    const.Font.main_2 = pygame.font.Font('Font/neodgm.ttf', 16)
+
+def load_image():
+    pass
 
 def main():
     while True:
